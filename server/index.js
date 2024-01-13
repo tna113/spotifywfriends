ddac0aa95453d9ea43ba761965259d1522d05ea2
ta7384 <ta7384@g.rit.edu>
2024-01-13 15:38:34 -0500

    connect db to backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
 //use express to make a simple web server
 const express = require("express");
-const PORT = process.env.PORT || 3001;
-const app = express();
+const mysql = require("mysql");
 
-app.get("/api", (req,res) => {
+const PORT = process.env.PORT || 3001;
+const DBHOST = "216.172.184.38";
+const DBUSER = "theaaria_admin";
+const DBPASS = "fluffyPancakes384";
+const DBNAME = "theaaria_spotifywfriends";
+
+const app = express();
+app.use(express.json());
+
+//create a connection pool to get next available connection instead of .createConnection, which creates a single, blocking connection
+const db = mysql.createPool({
+    host: DBHOST,
+    user: DBUSER,
+    password: DBPASS,
+    database: DBNAME,
+});
+
+app.get("/debug", (req,res) => {
+    db.query(
+        `SELECT * FROM user`, function (err,results) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                console.log(results);
+                return;
+            }
+        }
+    );
+
     res.status(200).json({mesage: 'hello werld'})
 })
 
