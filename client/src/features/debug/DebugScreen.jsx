5a72639c7d0ab59d8571343dd906dcacc5025897
Thea Arias <99890039+tna113@users.noreply.github.com>
2024-01-13 16:48:08 -0500

    create debug screen (#3)
    * debug screen

--- client/src/features/debug/DebugScreen.jsx
+++ client/src/features/debug/DebugScreen.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import Page from "../../components/Page";
+
+const buttonBox = {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: '1.5em',
+    marginBottom: '1.5em',
+}
+const apiBox = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '4px',
+}
 
 export default function DebugScreen() {
     return (
         <Page title="debug screen" subtitle="a screen for debugging/development purposes">
-            <p>content</p>
+            <div style={buttonBox}>
+                <Link to="/home">
+                    <Button variant="outlined">home screen</Button>
+                </Link>
+                <Button variant="outlined">login screen</Button>
+            </div>
+
+
+            <div style={apiBox}>
+                <Typography variant="h6">fetch api</Typography>
+                <div style={buttonBox}>
+                    <Button variant="outlined">fetch users</Button>
+                </div>
+                <textarea rows="20">output</textarea>
+            </div>
         </Page>
     )
 }
