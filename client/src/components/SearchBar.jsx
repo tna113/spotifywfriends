27c8e0b764e8e4b4d8cf344c63c913910881fc0d
ta7384 <ta7384@g.rit.edu>
2024-01-28 17:22:34 -0500

    exported get token function

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,12 +1,60 @@
 import { Container, FormControl, Input, InputLabel } from "@mui/material";
-import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from "axios";
+import React, { useState } from "react";
+
+const baseEndpoint = "https://api.spotify.com";
+const searchEndpoint = "/v1/search";
+
+//https://api.spotify.com/v1/search?q=remaster%2520track%3ADoxy%2520artist%3AMiles%2520Davis&type=track
+
+//https://developer.spotify.com/documentation/web-api/tutorials/getting-started
+
+//https://developer.spotify.com/documentation/web-api/concepts/api-calls
+
+//https://developer.spotify.com/documentation/web-api/reference/search
 
 export default function SearchBar() {
+    const [searchStr, setSearchStr] = useState("");
+    const handleSearchChange = (event) => {
+        event.preventDefault();
+        console.log(`seachStr: ${event.target.value}`);
+        setSearchStr(event.target.value);
+    }
+
+    const searchArtist = async (event) => {
+        event.preventDefault();
+
+        //get access token
+        const accessToken = "";
+
+        //define req headers
+        const axiosConfig = {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${accessToken}`,
+            }
+        }
+
+        //define PATH parameters
+        const params = {
+            q: searchStr,
+            type: "track",
+        }
+
+        //use axios to make reqest
+        axios.get(`${baseEndpoint}${searchEndpoint}`, {})
+            .then(response => {
+                
+            })
+            .catch((err) => ([{error: err}]));
+    }
+
     return (
         <Container>
             <FormControl fullWidth>
                 <InputLabel>Search</InputLabel>
-                <Input placeholder="Search" />
+                <Input placeholder="Search" defaultValue={searchStr} onChange={(event) => handleSearchChange(event)} />
             </FormControl>
         </Container>
     )
--- client/src/features/debug/DebugScreen.jsx
+++ client/src/features/debug/DebugScreen.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { Stack } from "@mui/system";
 import Page from "../../components/Page";
+import getAccessToken from "../../utils/getAccessToken";
 
 const buttonBox = {
     display: 'flex',
@@ -51,30 +52,11 @@ export default function DebugScreen() {
     };
 
     //get access token
-    const getToken = async (event) => {
-        event.preventDefault();
-
-        //define headers, parameters
-        const axiosConfig = {
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        };
-
-        const json = {
-            grant_type: "client_credentials",
-            client_id: "4f75feb86a3d49adb5a7df64f4d8780d",
-            client_secret: "ee257029ff0a4e999bd831c347bca096",
-        };
-
-        const data = new URLSearchParams(Object.entries(json)).toString();
-
-        //use axios to make post request
-        axios.post('https://accounts.spotify.com/api/token', data, axiosConfig)
-            .then(response => {
-                setOutput(JSON.stringify(response.data));
-            })
-            .catch(err => ([{error: err}]));
+    const getToken = (event) => {
+        const query = getAccessToken(event);
+        query.then(response => {
+            setOutput(response);
+        })
     }
 
     return (
