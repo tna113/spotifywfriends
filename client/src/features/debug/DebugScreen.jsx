cb3b54d7d2eabc3a59fac39ddbb976c51993f447
thea a <ta7384@g.rit.edu>
2024-02-06 20:25:56 -0500

    debug load all songs from db

--- client/src/features/debug/DebugScreen.jsx
+++ client/src/features/debug/DebugScreen.jsx
@@ -50,6 +50,31 @@ export default function DebugScreen() {
             .catch((err) => ([{error: err}]))
     };
 
+    const fetchSongs = async (event) => {
+        event.preventDefault();
+    
+        const axiosConfig = {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            }
+        };
+    
+        //TODO: handle roles
+        const params = {
+            role: "admin"
+        }
+        
+        axios.get(`http://localhost:3001/debug/songs`, {params, axiosConfig})
+            .then(response => {
+                const jsonSongs = JSON.stringify(response.data.songs);
+                setOutput(jsonSongs);
+            })
+    
+            //TODO: handle errors, alert snackboxes?
+            .catch((err) => ([{error: err}]))
+    };
+
     //get access token
     const getToken = async (event) => {
         event.preventDefault();
@@ -94,6 +119,11 @@ export default function DebugScreen() {
                     <div style={buttonBox}>
                         <Button variant="outlined" onClick={(event) => fetchUsers(event)}>fetch users</Button>
                     </div>
+
+                    <div style={buttonBox}>
+                        <Button variant="outlined" onClick={(event) => fetchSongs(event)}>fetch songs</Button>
+                    </div>
+
                     <div style={buttonBox}>
                         <Button variant="outlined" onClick={(event) => getToken(event)}>fetch spotify token</Button>
                     </div>
